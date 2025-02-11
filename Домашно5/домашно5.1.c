eee20ee7df297c92846e36ce376bcf91bff2cf14
Denislav-Miroslavov-Maksimov <deni.maximow@gmail.com>
2025-02-11 16:48:34 +0200

    Create домашно5.1.c

---
+++ Домашно5/домашно5.1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Available functions:\n");
+    printf("1. strlength - калкулира дължината на стринг\n");
+    printf("2. strconcat - смесва два стринга.\n");
+    printf("3. strcompare - сравнява два стринга\n");
+    return 0;
+}
